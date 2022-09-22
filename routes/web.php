95c95c6e11bc7b3b85a7247ca54b704d7fcd5861
Sensen <senoagung27@gmail.com>
2022-09-22 14:33:51 +0700

    frontend and landingpages

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'HomeController@index');
 
 Route::prefix('admin')
     ->namespace('Admin')
