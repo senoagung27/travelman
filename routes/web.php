b9673e824d1caee8f826c601312070de47a5786b
Sensen <senoagung27@gmail.com>
2022-09-23 13:14:25 +0700

    install and config auth

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,7 +25,10 @@ Route::get('/checkout/success', 'CheckoutController@success')->name('checkout-su
 
 Route::prefix('admin')
     ->namespace('Admin')
+    ->middleware(['auth', 'admin'])
     ->group(function () {
         Route::get('/', 'DashboardController@index')
         ->name('dashboard');
 });
+
+Auth::routes();
